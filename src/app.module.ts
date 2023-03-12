5318714e586020cbd767a4d03408c673896b1823
r.ryan <rick.ryan2@outlook.com>
2023-03-12 13:04:07 +0000

    feat: enable configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Logger } from './facilities/logging.service';
+import { Logger } from './facilities/facilities.logger';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    cache: true,
+    envFilePath: ['.env/.env.local', '.env/.env.dev', '.env/.env.prod'],
+    validationSchema: joi.object({
+      'NODE_ENV': joi.string()
+        .valid('local', 'development', 'production')
+        .default('local'),
+      'APP_ID': joi.string()
+        .required(),
+      'PORT': joi.number()
+        .default(3000),
+      'LOG_LEVEL': joi.string()
+        .valid('info', 'debug', 'error', 'warn', 'verbose')
+        .default('info'),
+      'BASE_PATH': joi.string()
+        .uri({ allowRelative: true })
+        .default('/'),
+    })
+  })],
   controllers: [AppController],
-  providers: [AppService, Logger],
+  providers: [AppService, ConfigService, Logger],
 })
 export class AppModule { }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,22 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { Logger } from './facilities/logging.service';
+import { Logger } from './facilities/facilities.logger';
 
 @Injectable()
 export class AppService {
   constructor(private readonly logger: Logger) { }
 
   getHello(): string {
-    this.logger.debug({ where: "AppService" }, 'hello is called');
-
-    this.logger.log({ where: "AppService" }, 'hello is called');
-
-    this.logger.error({
+    this.logger.verbose({
       something: 'happened',
       obj: {
         anything: 'happened'
       }
-    });
+    }, "this is a verbose");
 
+    this.logger.debug({ where: "AppService" }, 'hello is called');
+
+    this.logger.log({ where: "AppService" }, 'hello is called');
+
+    this.logger.warn('this is a warn');
+
+    this.logger.error(new Error('this is an error.'));
 
     return 'Hello World!';
   }
